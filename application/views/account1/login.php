f790a6340d721d3e3e0537c850a745810102b4a3
h4rz <darkking996@gmail.com>
2017-04-29 18:12:59 +0530

    Add files via upload

---
+++ application/views/account1/login.php
@@ -0,0 +1,61 @@
+<html>
+  <head>
+    <style>
+    </style>
+  </head>
+  <body>
+    <br><br><br><br><br><br><br><br>
+    <div class="row justify-content-center">
+      <div class="col-sm-5">   
+        <div class="card">
+          <div class="card-header text-center bg-primary text-white"><h3>Login</h3></div>
+          <div class="card-block">
+            <form>
+                <div class="form-group row">
+                  <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+                  <div class="col-sm-10">
+                    <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+                  </div>
+                </div>
+                <div class="form-group row">
+                  <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+                  <div class="col-sm-10">
+                    <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+                  </div>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-5">
+        <div class="card">
+          <div class="card-header text-center bg-primary text-white"><h3>Register</h3></div>
+          <div class="card-block">
+            <form>
+                <div class="form-group row">
+                  <label for="inputName" class="col-sm-2 col-form-label">Name</label>
+                  <div class="col-sm-10">
+                    <input type="text" class="form-control" id="inputName" placeholder="Name">
+                  </div>
+                </div>
+                <div class="form-group row">
+                  <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+                  <div class="col-sm-10">
+                    <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+                  </div>
+                </div>
+                <div class="form-group row">
+                  <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+                  <div class="col-sm-10">
+                    <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+                  </div>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
